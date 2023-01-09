4bb3b57bbb7f6f9b080254ee611aab2767559aca
Tim Pilius <tpilius@gmail.com>
2023-01-09 17:08:20 -0500

    Adding PrefillSummaryResult.cs and StopwatchExtensions.cs

---
+++ Extensions/StopwatchExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace LancachePrefill.Common.Extensions
+{
+    public static class StopwatchExtensions
+    {
+        //TODO document
+        public static string FormatElapsedString(this Stopwatch stopwatch)
+        {
+            var elapsed = stopwatch.Elapsed;
+            if (elapsed.TotalHours > 1)
+            {
+                return elapsed.ToString(@"h\:mm\:ss\.ff");
+            }
+            if (elapsed.TotalMinutes > 1)
+            {
+                return elapsed.ToString(@"mm\:ss\.ff");
+            }
+            return elapsed.ToString(@"ss\.ffff");
+        }
+    }
+}
---
+++ PrefillSummaryResult.cs
@@ -0,0 +1,54 @@
+﻿namespace LancachePrefill.Common
+{
+    public sealed class PrefillSummaryResult
+    {
+        public int AlreadyUpToDate { get; set; }
+        public int FailedApps { get; set; }
+        public int Updated { get; set; }
+        public int UnownedAppsSkipped { get; set; }
+
+        private int _totalGamesPrefilled => AlreadyUpToDate + FailedApps + Updated;
+        private Stopwatch PrefillElapsedTime { get; } = Stopwatch.StartNew();
+
+        public void RenderSummaryTable(IAnsiConsole ansiConsole)
+        {
+            var table = new Table
+            {
+                Border = TableBorder.MinimalHeavyHead
+            };
+
+            var rowFields = new List<string>();
+
+            // Number of updated apps
+            table.AddColumn(new TableColumn(Cyan("Updated")).Centered());
+            rowFields.Add(Updated.ToString());
+
+            // Apps already up to date
+            table.AddColumn(new TableColumn(Green("Up To Date")).Centered());
+            rowFields.Add(AlreadyUpToDate.ToString());
+
+            // Failed
+            if (FailedApps > 0)
+            {
+                table.AddColumn(new TableColumn(Red("Failed")).Centered());
+                rowFields.Add(FailedApps.ToString());
+            }
+
+            // Unowned
+            if (UnownedAppsSkipped > 0)
+            {
+                table.AddColumn(new TableColumn(LightYellow("Unowned")).Centered());
+                rowFields.Add(UnownedAppsSkipped.ToString());
+            }
+            table.AddRow(rowFields.ToArray());
+
+            var grid = new Grid()
+                       .AddColumn(new GridColumn())
+                       .AddRow($" Prefilled {Magenta(_totalGamesPrefilled)} apps in {LightYellow(PrefillElapsedTime.FormatElapsedString())}")
+                       .AddRow(table);
+
+            ansiConsole.Write(new Rule());
+            ansiConsole.Write(new Padder(grid, new Padding(1, 0)));
+        }
+    }
+}
