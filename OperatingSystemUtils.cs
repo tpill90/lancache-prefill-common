84b8e0dad3b3e96229d1a294b9daa65a7b1e1157
Tim Pilius <tpilius@gmail.com>
2023-01-14 19:28:03 -0500

    Adding utility to detect if an exe was double clicked on windows, so that a message can be displayed to the user.

---
+++ OperatingSystemUtils.cs
@@ -0,0 +1,31 @@
+﻿namespace LancachePrefill.Common
+{
+    public static class OperatingSystemUtils
+    {
+        //TODO document
+        public static void DetectDoubleClickOnWindows(string appName)
+        {
+#if OS_WINDOWS
+            var currentProcess = Process.GetCurrentProcess();
+            var parentProcess = ParentProcessUtilities.GetParentProcess(currentProcess.Id);
+
+            if (parentProcess.ProcessName != "explorer")
+            {
+                return;
+            }
+
+            AnsiConsole.Console.MarkupLine($"Detected that {Cyan(appName)} was started via double click!");
+            AnsiConsole.Console.MarkupLine($"{Cyan(appName)} is a command line app that must be started from a Powershell prompt.");
+            AnsiConsole.Console.MarkupLine("");
+            AnsiConsole.Console.MarkupLine("To run the app, open a Powershell prompt in this directory, " +
+                                           $"and start the app with {LightYellow($".\\{appName}.exe")}");
+
+            // Blocking on user input, so that they have a chance to read the output.
+            // No need to exit() the process here, it will immediately close itself anyway.
+            Console.ReadLine();
+#endif
+        }
+    }
+
+    // This code uses Windows kernel specific functions, shouldn't be included in compilation on other platforms
+}
