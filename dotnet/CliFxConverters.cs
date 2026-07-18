8557da4904ce3d6d2df80ecf1fc9b22cc45d86da
Tim Pilius <tpilius@gmail.com>
2026-07-17 20:00:50 -0400

    Removing NullableBoolConverter since it is no longer needed.

--- dotnet/CliFxConverters.cs
+++ dotnet/CliFxConverters.cs
@@ -1,20 +1,5 @@
 ﻿namespace LancachePrefill.Common
 {
-    // TODO Finish removing this in the other prefills.
-    public sealed class NullableBoolConverter : BindingConverter<bool?>
-    {
-        // When a [CommandOption] is a bool, it will always display 'Default "False"' in the --help text.  This is redundant and fatiguing to read, so
-        // switching the property over to bool? will make that text go away.  This however creates a new issue where passing the flag is no longer enough since "true",
-        // is no longer the default.
-        //
-        // What this essentially does is when a flag is specified but no value is provided it is (null), so the converter will return true to restore the original
-        // flag functionality.
-        public override bool? Convert(string rawValue)
-        {
-            return true;
-        }
-    }
-
     public sealed class TransferSpeedUnitConverter : BindingConverter<TransferSpeedUnit>
     {
         public override TransferSpeedUnit Convert(string rawValue)
